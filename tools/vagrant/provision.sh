fa659f6dd82295f0040e25d22c4af97dbefa73b8
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-03-06 00:45:47 +0100

    original fork plugins removed

--- tools/vagrant/provision.sh
+++ tools/vagrant/provision.sh
@@ -73,8 +73,6 @@ function install_roundcube() {
 }
 
 function install_roundcube_plugins() {
-  ln -s /vagrant/plugins/mailforward /var/lib/roundcube/plugins/
-  ln -s /vagrant/plugins/amavisspamsettings /var/lib/roundcube/plugins/
   ln -s /vagrant/plugins/twofactor_gauthenticator /var/lib/roundcube/plugins/
   rm /etc/roundcube/main.inc.php
   cp /vagrant/config/main.inc.php /etc/roundcube/
